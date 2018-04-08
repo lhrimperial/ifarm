b75173e6491f55b910bd9978ad44e3852d34ed88
lhr <lhr9563215@163.com>
2018-04-08 22:54:51 +0800

    ifarm-console

--- ifarm-console/console-facade/src/main/java/com/ifarm/console/facade/controller/UserController.java
+++ ifarm-console/console-facade/src/main/java/com/ifarm/console/facade/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.ifarm.console.facade.controller;
 
+import com.github.framework.server.shared.domain.vo.ResponseVO;
+import com.github.framework.server.web.AbstractController;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  **/
-public class UserController {
+@RestController
+public class UserController extends AbstractController{
+
+    public ResponseVO index() {
+        return returnSuccess();
+    }
 }
