c51509cd4fb28981c7eb3ef313e60c592b7d1710
lhr <lhr9563215@163.com>
2018-05-15 23:30:05 +0800

    ifarm-console

--- ifarm-console/console-startup/src/main/java/com/ifarm/console/Main.java
+++ ifarm-console/console-startup/src/main/java/com/ifarm/console/Main.java
@@ -3,7 +3,6 @@ package com.ifarm.console;
 import com.github.framework.starter.core.ApplicationContexts;
 import com.github.framework.starter.core.FrameBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 /**
  *
