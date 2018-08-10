9cad0d01c24a132de5e2e671035b9781f6624909
lhrimperial <lhr9563216@163.com>
2018-08-10 15:55:47 +0800

    ifarm-console

--- ifarm-console/console-facade/src/main/java/com/ifarm/console/facade/shiro/AccessHandlerFilter.java
+++ ifarm-console/console-facade/src/main/java/com/ifarm/console/facade/shiro/AccessHandlerFilter.java
@@ -1,6 +1,10 @@
 package com.ifarm.console.facade.shiro;
 
+import com.ifarm.console.shared.domain.dto.UserInfoDTO;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.apache.shiro.web.util.WebUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,6 +12,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -24,5 +29,17 @@ public class AccessHandlerFilter extends FormAuthenticationFilter {
             return false;
         }
         return super.preHandle(request, response);
+    }
+
+    @Override
+    protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
+        //获取已登录的用户信息
+        UserInfoDTO activeUser = (UserInfoDTO) subject.getPrincipal();
+        //获取session
+        HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+        HttpSession session = httpServletRequest.getSession();
+        //把用户信息保存到session
+        session.setAttribute("activeUser", activeUser);
+        return super.onLoginSuccess(token, subject, request, response);
     }
 }
